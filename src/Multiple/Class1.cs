a9bd9ea42f9a71904dbd14719dc34e27043f18d7
Renato Golia <renato.golia@outlook.com>
2021-01-21 19:12:44 +0100

    First commit

---
+++ src/Multiple/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TestAPI;
+
+namespace Test
+{
+    public class Class1
+    {
+        public async Task DoSomething()
+        {
+            var http = new HttpClient();
+            ITestClient testClient = new TestClient(http);
+            
+            var response = await testClient.EchoAsync(new Request { Message = "Hello World" });
+
+            Console.WriteLine($"Received: {response.Message}");
+        }
+    }
+}
